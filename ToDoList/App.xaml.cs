35c4044ad44b6c49e04d4fca4a84468ac0152e8d
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2025-09-02 20:02:42 -0600

    Create the OnStartUp method on the App class

--- ToDoList/App.xaml.cs
+++ ToDoList/App.xaml.cs
@@ -15,5 +15,12 @@ namespace ToDoList
     /// </summary>
     public partial class App : Application
     {
+        //This method executes everytime the app startsup
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //CREATE A DATABASE FILE IF IT DOESNT EXIST
+            DatabaseFacade facade = new DatabaseFacade(new DataContext());
+            facade.EnsureCreated();//This method will ensure that the database file is created if it doesnt exist
+        }
     }
 }
